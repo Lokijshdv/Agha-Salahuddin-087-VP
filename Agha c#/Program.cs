bd6eea876535685a4063a477909fe240f8ea7297
Dar Laptop <Dar Laptop@DESKTOP-ESRR84C>
2024-09-17 01:58:26 -0700

    Add project files.

---
+++ Agha c#/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+class Agha
+{
+    static void Main(string[] args)
+    {
+       
+        Console.WriteLine("Enter your Name: ");
+        string name = Console.ReadLine();
+
+        Console.WriteLine("Enter your Roll No: ");
+        string rollNo = (Console.ReadLine());
+
+        Console.Write("Enter your Class: ");
+        string studentClass = (Console.ReadLine());
+
+        Console.Write("Enter Section: ");
+        string section = Console.ReadLine();
+
+        Console.WriteLine("\nStudent Information:");
+        Console.WriteLine("Your Name: " + name);
+        Console.WriteLine("Your Roll No: " + rollNo);
+        Console.WriteLine("Your Class: " + studentClass);
+        Console.WriteLine("Your Section: " + section);
+    }
+}
